10c4a7be85ffcf0f4ef8b24d588064a860596c6f
NISSAN-SUMI\sumi <scheming_madness@yahoo.co.jp>
2024-01-10 11:55:25 -0500

    modify export file format

--- src/stella_vslam/io/trajectory_io.cc
+++ src/stella_vslam/io/trajectory_io.cc
@@ -96,6 +96,17 @@ void trajectory_io::save_frame_trajectory(const std::string& path, const std::st
                 << trans_wc(0) << " " << trans_wc(1) << " " << trans_wc(2) << " "
                 << quat_wc.x() << " " << quat_wc.y() << " " << quat_wc.z() << " " << quat_wc.w() << std::endl;
         }
+        else if (format == "OWN") {
+            const Mat33_t& rot_wc = cam_pose_wc.block<3, 3>(0, 0);
+            const Vec3_t& trans_wc = cam_pose_wc.block<3, 1>(0, 3);
+            const Quat_t quat_wc = Quat_t(rot_wc);
+            ofs << std::setprecision(15)
+                << frm_id << " "
+                << timestamps.at(frm_id) << " "
+                << std::setprecision(9)
+                << trans_wc(0) << " " << trans_wc(1) << " " << trans_wc(2) << " "
+                << quat_wc.x() << " " << quat_wc.y() << " " << quat_wc.z() << " " << quat_wc.w() << std::endl;
+        }
         else {
             throw std::runtime_error("Not implemented: trajectory format \"" + format + "\"");
         }
